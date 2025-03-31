55a2be1d341ee28c32b7892940090ad25c5df17d
Abdulla Bagishev <92680131+Abdullah-ElPsyKo@users.noreply.github.com>
2025-04-01 01:40:44 +0200

    Update prevent_empty_pw.sh

--- modules/prevent_empty_pw.sh
+++ modules/prevent_empty_pw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "🔒 Disabling empty password logins..."
+echo "Disabling empty password logins..."
 
 if grep -q "^PermitEmptyPasswords" /etc/ssh/sshd_config; then
     sudo sed -i 's/^PermitEmptyPasswords.*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
@@ -8,7 +8,7 @@ else
     echo "PermitEmptyPasswords no" | sudo tee -a /etc/ssh/sshd_config
 fi
 
-echo "🔁 Restarting SSH service..."
+echo "Restarting SSH service..."
 sudo systemctl restart sshd
 
-echo "✅ Empty password logins disabled."
+echo "Empty password logins disabled."
